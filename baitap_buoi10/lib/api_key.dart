107feea182aa22ca47dbe9e8e63cf71b36cd8da2
tuanluvcats <patuann15@gmail.com>
2026-05-23 11:22:39 +0700

    Untrack api_key.dart để bảo mật

--- baitap_buoi10/lib/api_key.dart
+++
@@ -1,7 +0,0 @@
-// Replace with your real Google Maps API key.
-// Also update the value in:
-//   android/app/src/main/AndroidManifest.xml -> com.google.android.geo.API_KEY
-const String kGoogleApiKey = "AIzaSyBvGzbhfXY9mGF4dcxbsnv9fwuJVb8z4qk";
-
-const String kGoogleDirectionsApiKey =
-    "AIzaSyAUdZW6mHZXDe8oCp9Vx6CnQCm7wu1_GeA";
